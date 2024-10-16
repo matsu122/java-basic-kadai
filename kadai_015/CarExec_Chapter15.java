93028dd6fa7228ea3f1cf6633a8fa383c638e6ba
matsu122 <ymatsumura@aicom.co.jp>
2024-10-16 13:54:53 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,12 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+	public static void main(String[] args) {
+		Car_Chapter15 gia =new Car_Chapter15();
+		gia.gearChange(3);
+		gia.run();
+	}
+		 
+}
+
+	
